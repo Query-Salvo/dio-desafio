31ca881f544872879cece0895de60433237d3797
Query-Salvo <thiagofaria1945@gmail.com>
2022-02-28 18:35:33 -0300

    terminado desafios de código

---
+++ javascript/Desafios de Código/desafio3.js
@@ -0,0 +1,8 @@
+let lines = gets().split("\n");
+
+let n = parseInt(lines.shift());
+//escreva o seu código aqui
+
+const frase = "LIFE IS NOT A PROBLEM TO BE SOLVED"
+
+console.log(frase.substr(0, n))
